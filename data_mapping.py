02a8df828a5238875281f802649d3c4bb94582ef
jose-ricardo-reis <133124692+jose-ricardo-reis@users.noreply.github.com>
2023-07-03 15:30:18 -0300

    Update data_mapping.py
    . Adicionou data atual no nome de cada arquivo para facilitar versionamento;
    . Remove o arquivo da Máquina Virtual utilizando "rm -r".

--- data_mapping.py
+++ data_mapping.py
@@ -4,6 +4,7 @@ import zipfile
 from google.cloud import bigquery
 from datetime import date
 
+
 def exportar_informacoes_do_dataset(id_projeto):
     # Cria um cliente BigQuery com o projeto fornecido
     cliente = bigquery.Client(project=id_projeto)
@@ -23,6 +24,7 @@ def exportar_informacoes_do_dataset(id_projeto):
         informacoes_do_dataset.append([nome_dataset, quantidade_tabelas])
 
     return informacoes_do_dataset
+
 
 def exportar_esquema_da_tabela(id_projeto, nome_dataset):
     # Cria um cliente BigQuery com o projeto fornecido
@@ -50,6 +52,7 @@ def exportar_esquema_da_tabela(id_projeto, nome_dataset):
 
     return esquemas_das_tabelas
 
+
 def exportar_quantidade_de_colunas(id_projeto, nome_dataset):
     # Cria um cliente BigQuery com o projeto fornecido
     cliente = bigquery.Client(project=id_projeto)
@@ -74,16 +77,19 @@ def exportar_quantidade_de_colunas(id_projeto, nome_dataset):
 
     return lista_quantidade_colunas
 
+
 def exportar_para_csv(data, nome_arquivo):
     # Exporta os dados para um arquivo CSV
     with open(nome_arquivo, 'w', newline='', encoding='utf-8') as arquivo:
         escritor = csv.writer(arquivo)
         escritor.writerows(data)
 
+
 def deletar_arquivos(nomes_arquivos):
     # Deleta os arquivos especificados
     for nome_arquivo in nomes_arquivos:
         os.remove(nome_arquivo)
+
 
 def compactar_pastas(pastas, nome_arquivo_zip):
     # Compacta as pastas em um arquivo ZIP
@@ -93,6 +99,7 @@ def compactar_pastas(pastas, nome_arquivo_zip):
                 for arquivo in arquivos:
                     caminho_arquivo = os.path.join(raiz, arquivo)
                     zipf.write(caminho_arquivo, os.path.relpath(caminho_arquivo, pasta))
+
 
 def compactar_arquivos(nomes_arquivos, nome_arquivo_zip):
     # Compacta os arquivos em um arquivo ZIP
@@ -106,13 +113,13 @@ def upload_para_bucket(bucket, nome_arquivo):
 
 def main(id_projeto):
     # Obtém a data atual
-    data_atual = date.today().strftime("%d-%m-%y")
+    data_atual = date.today().strftime("%Y-%m-%d")
     # Define o nome do arquivo ZIP final
-    nome_arquivo_zip_final = f"{id_projeto}_{data_atual}.zip"
+    nome_arquivo_zip_final = f"{data_atual}_{id_projeto}.zip"
 
     # Exporta as informações do dataset para um arquivo CSV
     datasets = exportar_informacoes_do_dataset(id_projeto)
-    arquivo_csv_datasets = f'{id_projeto}_datasets_{data_atual}.csv'
+    arquivo_csv_datasets = f'{data_atual}_{id_projeto}_datasets.csv'
     exportar_para_csv(datasets, arquivo_csv_datasets)
 
     # Lista para armazenar as tabelas dos datasets e as pastas dos datasets
@@ -125,7 +132,7 @@ def main(id_projeto):
         nome_dataset = dataset[0]
         # Exporta o esquema da tabela para um arquivo CSV
         esquemas_tabelas = exportar_esquema_da_tabela(id_projeto, nome_dataset)
-        arquivo_csv_tabelas = f'{id_projeto}_{nome_dataset}_tabelas_{data_atual}.csv'
+        arquivo_csv_tabelas = f'{data_atual}_{id_projeto}_{nome_dataset}_tabelas.csv'
         exportar_para_csv(esquemas_tabelas, arquivo_csv_tabelas)
         # Armazena o nome do dataset e o arquivo CSV das tabelas na lista correspondente
         tabelas_do_dataset.append((nome_dataset, arquivo_csv_tabelas))
@@ -141,37 +148,50 @@ def main(id_projeto):
         nome_dataset = dataset[0]
         # Exporta a quantidade de colunas para um arquivo CSV
         quantidade_colunas = exportar_quantidade_de_colunas(id_projeto, nome_dataset)
-        arquivo_csv_quantidade_colunas = f'{id_projeto}_{nome_dataset}_{data_atual}.csv'
+        arquivo_csv_quantidade_colunas = f'{data_atual}_{id_projeto}_{nome_dataset}.csv'
         exportar_para_csv(quantidade_colunas, arquivo_csv_quantidade_colunas)
         # Adiciona o arquivo CSV à lista correspondente
         arquivos_quantidade_colunas.append(arquivo_csv_quantidade_colunas)
 
     # Define os nomes dos arquivos ZIP
-    arquivo_zip_tabelas_dataset = f'{id_projeto}_tabelas_dataset_{data_atual}.zip'
-    arquivo_zip_quantidade_colunas = f'{id_projeto}_quantidade_colunas_{data_atual}.zip'
-    arquivo_zip_data_mapping = f'{id_projeto}_data_mapping_{data_atual}.zip'
+    arquivo_zip_tabelas_dataset = f'{data_atual}_{id_projeto}_tabelas-dataset.zip'
+    arquivo_zip_quantidade_colunas = f'{data_atual}_{id_projeto}_qtdade-colunas.zip'
+    arquivo_zip_data_mapping = f'{data_atual}_{id_projeto}.zip'
 
     # Compacta os arquivos das tabelas dos datasets em um arquivo ZIP
-    compactar_arquivos([arquivo_csv_tabelas for _, arquivo_csv_tabelas in tabelas_do_dataset], arquivo_zip_tabelas_dataset)
+    compactar_arquivos([arquivo_csv_tabelas for _, arquivo_csv_tabelas in tabelas_do_dataset],
+                       arquivo_zip_tabelas_dataset)
 
     # Compacta os arquivos CSV com a quantidade de colunas em um arquivo ZIP
     compactar_arquivos(arquivos_quantidade_colunas, arquivo_zip_quantidade_colunas)
 
     # Compacta os arquivos CSV, o arquivo ZIP das tabelas dos datasets e o arquivo ZIP da quantidade de colunas em um arquivo ZIP final
-    compactar_arquivos([arquivo_csv_datasets, arquivo_zip_tabelas_dataset, arquivo_zip_quantidade_colunas], arquivo_zip_data_mapping)
+    compactar_arquivos([arquivo_csv_datasets, arquivo_zip_tabelas_dataset, arquivo_zip_quantidade_colunas],
+                       arquivo_zip_data_mapping)
 
     # Deleta os arquivos CSV, os arquivos ZIP das tabelas dos datasets e o arquivo ZIP da quantidade de colunas
-    deletar_arquivos([arquivo_csv_datasets] + arquivos_quantidade_colunas + [arquivo_zip_tabelas_dataset, arquivo_zip_quantidade_colunas] + [nome_arquivo_tabela for _, nome_arquivo_tabela in tabelas_do_dataset])
+    deletar_arquivos([arquivo_csv_datasets] + arquivos_quantidade_colunas + [arquivo_zip_tabelas_dataset,
+                                                                             arquivo_zip_quantidade_colunas] + [
+                         nome_arquivo_tabela for _, nome_arquivo_tabela in tabelas_do_dataset])
 
     # Envia o arquivo ZIP final para o bucket GCS
-    upload_para_bucket('staging-datahub-provider-dsv', arquivo_zip_data_mapping)
+    upload_para_bucket(bucket, arquivo_zip_data_mapping)
+
+    # Apaga arquivo da VM
+    comando_rm = f"rm -r {data_atual}_{id_projeto}.zip"
+    os.system(comando_rm)
 
     print(f"Informações do dataset e tabelas exportadas para {arquivo_zip_data_mapping}")
 
+
 if __name__ == '__main__':
+    
+    ''' Lembre de alterar as variávis
+        id_projeto e bucket'''
+
     # Define o ID do projeto
-    id_projeto = 'Project_Id'
+    id_projeto = 'eneva-datahub-prv-dsv'
     # Define o caminho do bucket
-    bucket = 'Bucket_path/To_path'
+    bucket = 'staging-datahub-provider-dsv/Data_Mapping'
     # Chama a função principal
     main(id_projeto)
